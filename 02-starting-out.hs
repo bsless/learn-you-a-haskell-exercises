1355531a66ca4d2a89e6803814b77e112bb0b656
bsless <ben.sless@gmail.com>
2018-05-01 22:27:30 +0300

    Solve recursion, clean starting-out

--- 02-starting-out.hs
+++ 02-starting-out.hs
@@ -40,9 +40,9 @@ duplicate xs = concat [[x,x] | x <- xs]
  - For example "ziplike [1,2,3] ['a', 'b', 'c', 'd']" returns [(1,'a'), (2, 'b'), (3, 'c')]
  -}
 ziplike :: [a] -> [b] -> [(a, b)]
-ziplike [] xs = []
-ziplike xs [] = []
-ziplike (x:xs) (y:ys) = [(x,y)] ++ ziplike xs ys
+ziplike [] _ = []
+ziplike _ [] = []
+ziplike (x:xs) (y:ys) = (x,y):ziplike xs ys
 
 -- Split a list l at element k into a tuple: The first part up to and including k, the second part after k
 -- For example "splitAtIndex 3 [1,1,1,2,2,2]" returns ([1,1,1],[2,2,2])
@@ -62,7 +62,7 @@ slice i k l = drop i (take k l)
 -- Insert element x in list l at index k
 -- For example, "insertElem 2 5 [0,0,0,0,0,0]" returns [0,0,0,0,0,2,0]
 insertElem :: a -> Int -> [a] -> [a]
-insertElem x k l = (take k l) ++ [x] ++ (drop k l)
+insertElem x k l = (take k l) ++ x:(drop k l)
 
 -- Rotate list l n places left.
 -- For example, "rotate 2 [1,2,3,4,5]" gives [3,4,5,1,2]
--- 05-recursion.hs
+++ 05-recursion.hs
@@ -1,24 +1,31 @@
+main :: IO()
+main = undefined
 -- Raise x to the power y, using recursion
 -- For example, power 5 2 = 25
 power :: Int -> Int -> Int
-power x y = undefined
+power x 0 = 1
+power x y = x * power x (y - 1)
 
 -- create a list of length n of the fibbonaci sequence in reverse order
 -- examples: fib 0 = [0]
 -- 	     fib 1 = [1, 0]
---	     fib 10 = [55,34,21,13,8,5,3,2,1,1,0]	
+--	     fib 10 = [55,34,21,13,8,5,3,2,1,1,0]
 -- try to use a where clause
 fib :: (Num a, Eq a) => a -> [a]
-fib x = undefined
+fib 0 = [0]
+fib 1 = [1, 0]
+fib n = head prev + head prev2 : prev
+  where prev  = fib (n-1)
+        prev2 = fib (n-2)
 
 -- This is not recursive, but have a go anyway.
 -- Create a function which takes two parameters, a number and a step
 -- The result is the sign of the original number reversed, and the step added to the absolute value
 -- Confused? Some examples: stepReverseSign 6 2 = -8
---			    stepReverseSign -3 1 = 4
+--			    stepReverseSign (-3) 1 = 4
 --			    stepReverseSign 1 2 = -3
 stepReverseSign :: (Fractional a, Ord a) => a -> a -> a
-stepReverseSign a = undefined
+stepReverseSign num step = (if num > 0 then -1 else 1) * (abs(num) + step)
 
 {- Lets calculate pi.
  - The Leibniz formula for pi (http://en.wikipedia.org/wiki/Leibniz_formula_for_%CF%80)
@@ -36,7 +43,7 @@ stepReverseSign a = undefined
  - snd is the number of recursive steps taken to calculate it, after all this chapter is about recursion!
  - Example: piCalc 0.001 = (3.1420924036835256,2000)
 
- - The piCalc' function is defined as 
+ - The piCalc' function is defined as
  - piCalc' :: (Ord a, Fractional a, Integral b) => a -> a -> a -> b -> (a, b)
  - Lots of parameters!
  - The first parameter is the current denominator from the Leibniz formula
@@ -47,13 +54,15 @@ stepReverseSign a = undefined
  -
  - Feel free to change the parameter order, what parameters you need etc in order to get this to work for you,
  - But, of course the output of piCalc should remain as (pi, count)
- - 
+ -
  - You may find the stepReverseSign function handy
  -}
 
 piCalc :: (Fractional a, Integral b, Ord a) => a -> (a, b)
-piCalc a = undefined
+piCalc a = piCalc' 1 0.0 a 0
 
 piCalc' :: (Ord a, Fractional a, Integral b) => a -> a -> a -> b -> (a, b)
-piCalc' w x y z = undefined
-
+piCalc' denom prevPi tolerance count = if (abs(newPi - prevPi) < tolerance)
+                                       then (newPi, count)
+                                       else piCalc' (stepReverseSign denom 2) newPi tolerance ( count + 1 )
+                                       where newPi = prevPi + (4 / denom)
